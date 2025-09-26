0f54ece790fb93bbe008dc8a65ceb11b49a7a860
Math Application <xoawtaka@gmail.com>
2025-09-26 15:16:08 -0400

    Second commit.2 — unpolished

--- src/main/java/com/pluralsight/CalculatorApp.java
+++ src/main/java/com/pluralsight/CalculatorApp.java
@@ -1,6 +1,5 @@
 package com.pluralsight;
 
-import javax.management.AttributeList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,70 +8,52 @@ public class CalculatorApp {
     public static void main(String[] args) {
 
         //Use the scanner to capture user input
-        Scanner input = new Scanner(System.in); {
-        List<Double> numbers = new ArrayList<>();
-        List<String> operators = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        double total;
+        {
+            List<Double> numbers = new ArrayList<>();
+            List<String> operators = new ArrayList<>();
 
         /*Enter the First Number:
         Read the number user inputs
          */
-        System.out.print("Enter the first number: ");
-        numbers.add(input.nextDouble());
-
-        //Side Note: I want a more efficient reading of the number
-            // Read numbers until user decides to stop inputting:
-        while (!numbers.isEmpty()) {
-            System.out.print("Enter operation: (/, *, +, -)");
-            String operation = input.next();
-
-            if (operation.equals("=")) {
-                break; //User stops inputting — Calculator solves
-            }
-
-            operators.add(operation);
-            System.out.print("Enter the next number: ");
+            System.out.print("Enter the first number: ");
             numbers.add(input.nextDouble());
-        }
-        //Include other operating systems for output:
-        double total = 0;
-            for (Double aDouble : numbers) {
-                String operation = aDouble.toString();
-                double nextNumber = aDouble;
+
+            //Side Note: I want a more efficient reading of the number
+            // Read numbers until user decides to stop inputting:
+            while (!numbers.isEmpty()) {
+                System.out.print("Enter operation: (+, -, *, /, =) ");
+                String operation = input.next();
+
+                if (operation.equals("=")) {
+                    break; //User stops inputting — Calculator solves
+                }
+
+                operators.add(operation);
+                System.out.print("Enter the next number: ");
+                numbers.add(input.nextDouble());
+            }
+            //Include other operating systems for output:
+            total = numbers.getFirst();
+            for (int i = 0; i < operators.size(); i++) {
+                String operation = operators.get(i);
+                double nextNumb = numbers.get(i);
+
 
                 switch (operation) {
+                    case "+" -> total += nextNumb;
+                    case "-" -> total -= nextNumb;
+                    case "*" -> total *= nextNumb;
                     case "/" -> {
-                        double number = nextNumber;
-                        while (number > 0) {
-                            number = number / number;
-                            total = total + number;
+                        if (nextNumb == 0) {
+                            throw new ArithmeticException("Cannot divide by zero: Undefined");
                         }
+                        total /= nextNumb;
                     }
-                    case "*" -> {
-                        double number = nextNumber;
-                        while (number > 0) {
-                            number = number * number;
-                            total = total + number;
-                        }
-                    }
-                    case "+" -> {
-                        double number = nextNumber;
-                        while (number > 0) {
-                            number = number + number;
-                            total = total + number;
-                        }
-                    }
-                    case "-" -> {
-                        double number = nextNumber;
-                        while (number > 0) {
-                            number = number - number;
-                            total = total - number;
-                        }
-                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + operation);
                 }
-            }
-
-
-
+            } System.out.println("Equals: " + total);
         }
     }
 }
